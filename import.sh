03705ac4d2cadf48feaac45ff384dd28c3986bce
b-s-code <89502375+b-s-code@users.noreply.github.com>
2024-08-17 17:46:55 +0800

    clear profiles before import in script, add clearer output to user

--- import.sh
+++ import.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-echo Attempting to import GNOME Terminal profile with Zenburn colour scheme.
+echo Deleting all GNOME Terminal profiles, then activating default.
+dconf reset -f /org/gnome/terminal/legacy/profiles:/
+
+echo Attempting to import and activate GNOME Terminal profile with Zenburn colour scheme.
 dconf load /org/gnome/terminal/legacy/profiles:/ < gnomeProfile.dconf
+
+echo If Zenburn GNOME Terminal profile import and activation was successful, Zenburn colour should now be visible.
