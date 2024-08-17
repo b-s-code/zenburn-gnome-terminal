bf858b4c65d781c490cde0aad0c3d047e4f540aa
b-s-code <89502375+b-s-code@users.noreply.github.com>
2024-08-17 17:50:57 +0800

    quote input to echo

--- import.sh
+++ import.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo Deleting all GNOME Terminal profiles, then activating default.
+echo "Deleting all GNOME Terminal profiles, then activating default."
 dconf reset -f /org/gnome/terminal/legacy/profiles:/
 
-echo Attempting to import and activate GNOME Terminal profile with Zenburn colour scheme.
+echo "Attempting to import and activate GNOME Terminal profile with Zenburn colour scheme."
 dconf load /org/gnome/terminal/legacy/profiles:/ < gnomeProfile.dconf
 
-echo If Zenburn GNOME Terminal profile import and activation was successful, Zenburn colour should now be visible.
+echo "If Zenburn GNOME Terminal profile import and activation was successful, Zenburn colour should now be visible."
