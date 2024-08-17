d6619776917d2a1f4f6433ef6c100cc1eb3b60ce
b-s-code <89502375+b-s-code@users.noreply.github.com>
2024-08-17 17:38:59 +0800

    import actually working

--- import.sh
+++ import.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-echo Attempting to import.
-dconf load /org/gnome/terminal/legacy/profiles:/:f6fb5d43-4547-4562-adc1-fae78bdc73a/ < gnomeProfile.dconf
-
+echo Attempting to import GNOME Terminal profile with Zenburn colour scheme.
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnomeProfile.dconf
